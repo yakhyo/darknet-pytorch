04c526f32b8f5e6a3d23701f5e7b994e772efdc4
Yakhyokhuja Valikhujaev <yakhyo9696@gmail.com>
2021-10-14 00:35:29 +0900

    Update nn.py

--- nets/nn.py
+++ nets/nn.py
@@ -9,7 +9,7 @@ class Conv(nn.Module):
         super(Conv, self).__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, auto_pad(k, p), groups=g, bias=False)
         self.bn = nn.BatchNorm2d(c2)
-        self.act = nn.LeakyReLU() if act else nn.ReLU()
+        self.act = nn.LeakyReLU(0.1, inplace=True) if act else nn.ReLU(inplace=True)
 
     def forward(self, x):
         return self.act(self.bn(self.conv(x)))
